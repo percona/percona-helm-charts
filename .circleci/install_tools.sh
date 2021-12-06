aa5dff24d4d070af0c06d8763cb3a1730e7a8d7e
Mykola Marzhan <mykola.marzhan@percona.com>
2021-12-06 13:54:42 +0200

    comment wrong URL

--- .circleci/install_tools.sh
+++ .circleci/install_tools.sh
@@ -6,7 +6,7 @@ readonly HELM_VERSION=2.13.1
 readonly CHART_RELEASER_VERSION=0.1.4
 
 echo "Installing Helm..."
-curl -LO "https://kubernetes-helm.storage.googleapis.com/helm-v$HELM_VERSION-linux-amd64.tar.gz"
+#curl -LO "TODO: add correct url to helm-v$HELM_VERSION-linux-amd64.tar.gz"
 sudo mkdir -p "/usr/local/helm-v$HELM_VERSION"
 sudo tar -xzf "helm-v$HELM_VERSION-linux-amd64.tar.gz" -C "/usr/local/helm-v$HELM_VERSION"
 sudo ln -s "/usr/local/helm-v$HELM_VERSION/linux-amd64/helm" /usr/local/bin/helm
