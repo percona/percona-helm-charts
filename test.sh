09ea9759123ec91a5193623bd9ea00f3369085f2
Mayank Shah <mayank.shah@percona.com>
2024-11-10 00:11:17 +0530

    test
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- test.sh
+++
@@ -1,7 +0,0 @@
-if git diff --exit-code > /dev/null && [ -z "$(git ls-files --others --exclude-standard)" ]; then
-    echo "Dependencies are up to date."
-else
-    echo "Dependencies are out of date. Please run 'make deps' and commit the changes."
-exit 1
-fi
-
